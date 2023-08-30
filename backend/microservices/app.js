a18e7e1bfb4de7a5eac5bfb59f67cc92ee40de23
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-08-30 23:30:57 +0530

    [ADDED] express.static

--- backend/microservices/app.js
+++ backend/microservices/app.js
@@ -4,12 +4,12 @@ import { config } from "dotenv";
 import gameRouter from "./routes/games.js"
 import {errorMiddleware} from "./middlewares/error.js";
 import cors from "cors"
+import path from "path";
 
 //* Connecting to DB
 connectDB();
 
 export const app = express();
-// config();
 
 //Using middlewares
 app.use(express.json());
@@ -20,12 +20,17 @@ app.use(
         credentials: true,
     }));
 
+app.use(express.static(path.resolve("./public")));
+
 //using routes
 app.use("/games", gameRouter );
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+"/index.html");
+    const filePath = '/public/index.html';
+    res.sendFile(filePath);
 });
+
+app.listen(process.env.PORT,() => console.log(`listening on http://localhost:${process.env.PORT} in ${process.env.NODE_ENV} mode`));
 
 app.use(errorMiddleware);
 
--- backend/microservices/database/database.js
+++ backend/microservices/database/database.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 config();
 
 export const connectDB = () => {
-    mongoose.connect(process.env.MONGO_URI, {dbName: "microservices", useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(process.env.MONGO_URI, {dbName: "microservice", useNewUrlParser: true, useUnifiedTopology: true})
     .then((c) => {
         console.log(`Database connected with ${c.connection.host}`);
     })
