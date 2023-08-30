5b1246169498646a303b5f362bc9848ba7d1eb07
kuv2707 <kislayuv27@gmail.com>
2023-08-30 21:22:39 +0530

    basic database and socket impl

---
+++ backend/microservices/socket.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const socketIo = require("socket.io");
+const gameData=require("gameData")
+
+export default function (expressApp) {
+	const server = http.createServer(expressApp);
+	const io = socketIo(server);
+
+	io.on("connection", (socket) => {
+		console.log("A user connected");
+
+		socket.on("disconnect", () => {
+			console.log("A user disconnected");
+		});
+	});
+
+	server.listen(3000, () => {
+		console.log("Socket.IO server listening on port 3000");
+	});
+
+	return function()
+	{
+		io.sockets.broadcast("allData",gameData.getData());
+	};
+}
