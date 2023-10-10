35996c93c3c93544bd2d4884fa6abacf21dde701
Kislay <88523649+kuv2707@users.noreply.github.com>
2023-10-10 23:47:13 +0530

    [ADDED] logo mapping

--- frontend/src/components/LandingPages/matches/Fixtures.js
+++ frontend/src/components/LandingPages/matches/Fixtures.js
@@ -2,6 +2,53 @@ import React from 'react';
 import './ResultFixture.css';
 import cricket_img from '../Events/image/crickethover.png';
 import badminton_img from '../Events/image/badmintonhover.png';
+import football_img from '../Events/image/footballhover.png';
+import tennis_img from '../Events/image/tennishover.png';
+import volleyball_img from '../Events/image/volleyballhover.png';
+import basketball_img from '../Events/image/basketballhover.png';
+import tabletennis_img from '../Events/image/tabletennishover.png';
+import chess_img from '../Events/image/chesshover.png';
+//handball,hockey,squash
+import handball_img from '../Events/image/handballhover.png';
+import hockey_img from '../Events/image/hockeyhover.png';
+import squash_img from '../Events/image/squashhover.png';
+//powerlifting, kabaddi, weightlifting, taekwondo
+import powerlifting_img from '../Events/image/powerliftinghover.png';
+import kabaddi_img from '../Events/image/kabaddihover.png';
+import weightlifting_img from '../Events/image/weightliftinghover.png';
+import taekwondo_img from '../Events/image/taekwondohover.png';
+//aquatics,athletics,boxing,cycling,khokho,tabletennis
+import aquatics_img from '../Events/image/aquaticshover.png';
+import athletics_img from '../Events/image/athleticshover.png';
+import boxing_img from '../Events/image/boxinghover.png';
+import cycling_img from '../Events/image/cyclinghover.png';
+import khokho_img from '../Events/image/kho-khohover.png';
+
+const gameLogoMap = {
+  Cricket: cricket_img,
+  Badminton: badminton_img,
+  Football: football_img,
+  Tennis: tennis_img,
+  Volleyball: volleyball_img,
+  Basketball: basketball_img,
+  "Table Tennis": tabletennis_img,
+  Chess: chess_img,
+  Handball: handball_img,
+  Hockey: hockey_img,
+  Squash: squash_img,
+  Powerlifting: powerlifting_img,
+  Kabaddi: kabaddi_img,
+  "Weight Lifting": weightlifting_img,
+  Taekwondo: taekwondo_img,
+  Aquatics: aquatics_img,
+  Athletics: athletics_img,
+  Boxing: boxing_img,
+  Cycling: cycling_img,
+  "Kho-kho": khokho_img,
+}
+
+
+
 //TODO: import all
 const Fixtures = ({ selectedSport,games,date }) => {
   let match=(selectedSport==="All"?games:games.filter(game=>game.game_name===selectedSport)).filter(game=>game.game_start===date)
@@ -15,7 +62,7 @@ const Fixtures = ({ selectedSport,games,date }) => {
           <div className="row1">
             <div style={{ display: 'flex' }}>
               <img
-                src={badminton_img}
+                src={gameLogoMap[data.game_name]??cricket_img}
                 style={{ height: '25px' }}
                 alt="sport icon"
               />
